ed9c5a66a3d999fda68b1c8c48d07b1cbeade058
Md Kaif Khan <kaif.18cs330@citranchi.ac.in>
2023-07-23 14:19:21 +0530

    React App

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -31,10 +31,7 @@ const HomeContainer = () => {
         </p>
 
         <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima velit
-          eaque fugit distinctio est nam voluptatum architecto, porro iusto
-          deserunt recusandae ipsa minus eos sunt, dolores illo repellat facere
-          suscipit!
+        "Satisfy your cravings with just a click - Order now and indulge in a delightful culinary experience through our wide selection of delectable dishes!"
         </p>
 
         <button
